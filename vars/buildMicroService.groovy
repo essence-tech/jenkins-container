7ee29d5bba52f5a0290b091a80a1e4f8c724e8b8
michel laporte <michel.laporte@L452MALT.local>
2018-05-15 15:25:55 +0100

    changed service

--- vars/buildMicroService.groovy
+++ vars/buildMicroService.groovy
@@ -1,4 +1,4 @@
 def buildService(String imageName, String tag, String service) {
 	echo "Building ${imageName} with tag ${tag}"
-	sh 'docker build -t "${env.imageName}":"${env.tag}" -f "${env.service}"/Dockerfile'
+	sh "docker build -t ${env.imageName}:${env.tag} -f ${env.service}/Dockerfile"
 }
