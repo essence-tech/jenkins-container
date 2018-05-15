6d730f7cb2a24761c2f35fbcdff32bbbdb1de958
michel laporte <michel.laporte@L452MALT.local>
2018-05-15 15:33:37 +0100

    added docker build

--- vars/buildMicroService.groovy
+++ vars/buildMicroService.groovy
@@ -1,4 +1,4 @@
 def buildService(String imageName, String tag, String service) {
 	echo "Building ${imageName} with tag ${tag}"
-	sh "docker build -t ${imageName}:${tag} -f ${service}/Dockerfile ."
+	def customImage = docker.build(\""${imageName}"\", \"./"${service}"/Dockerfile\")
 }
