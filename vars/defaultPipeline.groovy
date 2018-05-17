1398a541394a8c6f22d3783faa6f1b8fd6b21ecd
michel laporte <michel.laporte@L452MALT.local>
2018-05-17 22:26:59 +0100

    added defaultPipeline

---
+++ vars/defaultPipeline.groovy
@@ -0,0 +1,43 @@
+ def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        def buildTests(){
+        	echo "MAKE TEST"
+        }
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                // stage ('Clone') {
+                //     checkout scm
+                // }
+                stage ('Build') {
+                	buildTests()
+                    // sh "echo 'building ${config.projectName} ...'"
+                }
+                stage ('Tests') {
+                    parallel 'static': {
+                        sh "echo 'shell scripts to run static tests...'"
+                    },
+                    'unit': {
+                        sh "echo 'shell scripts to run unit tests...'"
+                    },
+                    'integration': {
+                        sh "echo 'shell scripts to run integration tests...'"
+                    }
+                }
+                stage ('Deploy') {
+                    sh "echo 'deploying to server ${config.serverDomain}...'"
+                }
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
