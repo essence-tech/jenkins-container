f4e129cf9e9a4a9a79305638256b8b3e88076b5e
michel laporte <michel.laporte@L452MALT.local>
2018-06-06 11:50:49 +0100

    addded delegate

--- vars/testScript.groovy
+++ vars/testScript.groovy
@@ -1,5 +1,9 @@
 // vars/testScript.groovy
-def call(Map config) {
+def call(body) {
     // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
     sh "echo ${config.gitTag} is with ${config.gitCommit}"
   }
