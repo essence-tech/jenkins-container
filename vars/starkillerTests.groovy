ad8cda7c5762c2937bc8ad90d9b0776261681c87
michel laporte <michel.laporte@L452MALT.local>
2018-06-07 14:41:48 +0100

    s-

--- vars/starkillerTests.groovy
+++ vars/starkillerTests.groovy
@@ -16,8 +16,8 @@ def runTest(Map config) {
 }
 
 def runAll() {
-	//def microServicesList = "${microServices}"
-	println env.microServicesList
+	//def microServicesList = "[frontend]"
+	println "${}"+env.microServicesList
 	env.microServicesList.each {
 	runTest(test:'bdd_tests')
 	}
