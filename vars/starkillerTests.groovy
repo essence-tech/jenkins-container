f0abe6aee43ffca189ce5b25b056da0465b73b84
michel laporte <michel.laporte@L452MALT.local>
2018-06-07 16:00:16 +0100

    added all tests

--- vars/starkillerTests.groovy
+++ vars/starkillerTests.groovy
@@ -1,25 +1,17 @@
-// def runTest(Map config) {
-// 	dir ("${params.microserviceOption}") {
-// 	sh " ls && make ${config.test}"
-// 	}
-// }
-
-// def runAll(Map config) {
-// 	sh "echo running all tests && make ${config.test}"
-// 	}
-// }
 def runTest(Map config) {
 	def service = "${params.microserviceOption}"
-	dir ("${params.microserviceOption}") {
+	dir ("${service}") {
 		sh "ls && make ${config.test}"
 	}
 }
 
 def runAll() {
-	//def microServicesList = "[frontend]"
+	//Make a list of strings
 	String[] ms = microServicesList.split(',')
-	println ms.getClass()
 	ms.each {
-	runTest(test:'bdd_tests')
+		//runTest(test:'bdd_tests')
+		dir ("${ms}") {
+			sh "ls && make bdd_tests"
+		}
 	}
 }
