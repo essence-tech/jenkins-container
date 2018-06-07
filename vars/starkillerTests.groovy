9bea285fdef13aeaa206af7f93e376f4bc537554
michel laporte <michel.laporte@L452MALT.local>
2018-06-07 11:02:27 +0100

    added new test

--- vars/starkillerTests.groovy
+++ vars/starkillerTests.groovy
@@ -1,5 +1,10 @@
-def call(Map config) {
+def runTest(Map config) {
 	dir ("${params.microserviceOption}") {
 	sh " ls && make ${config.test}"
+	}
+}
+
+def runAll(Map config) {
+	sh "echo Running all tests && make ${config.test}"
 	}
 }
