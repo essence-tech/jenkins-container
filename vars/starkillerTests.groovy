1cb744d3d94b3a509fd19b6589e34a8b3ce7cd5a
michel laporte <michel.laporte@L452MALT.local>
2018-06-07 13:11:49 +0100

    added wrappers

--- vars/starkillerTests.groovy
+++ vars/starkillerTests.groovy
@@ -1,9 +1,25 @@
+// def runTest(Map config) {
+// 	dir ("${params.microserviceOption}") {
+// 	sh " ls && make ${config.test}"
+// 	}
+// }
+
+// def runAll(Map config) {
+// 	sh "echo running all tests && make ${config.test}"
+// 	}
+// }
+
 def runTest(Map config) {
-	dir ("${params.microserviceOption}") {
-	sh " ls && make ${config.test}"
+	//arg = "${params.microserviceOption}"
+	def service = "${params.microserviceOption}"
+	dir (service) {
+		sh " ls && make ${config.test}"
 	}
 }
 
-def runAll(Map config) {
-	sh "echo Running all tests && make ${config.test}"
+def runAll() {
+	def microServicesList = ["frontend"]
+	for (item in microServicesList) {
+	runTest(test:'bdd_tests')
 	}
+}
