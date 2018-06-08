b36c6d596f47c8bb085e9b2f76b21d0fba8a5ae0
michel laporte <michel.laporte@L452MALT.lan>
2018-06-08 10:01:56 +0100

    change scope of tests

--- vars/starkillerTests.groovy
+++ vars/starkillerTests.groovy
@@ -1,15 +1,26 @@
-def runTest(Map config) {
-	def service = "${params.microserviceOption}"
-	dir ("${service}") {
-		sh "ls && make ${config.test}"
+//This is the main logic to run the test.
+// def runTest(serviceName='') {
+// 	def service = "${params.microserviceOption}"
+// 	if(serviceName?.trim()) {
+//     service = "${serviceName}"
+// 	}
+// 	dir ("${service}") {
+// 		sh "ls && make ${config.test}"
+// 	}
+// }
+def runTest(serviceName, testName) {
+	dir ("${serviceName}") {
+		sh "make ${testName}"
 	}
 }
+
+
 
 def runAll() {
 	//Make a list of strings
 	String[] ms = microServicesList.split(',')
 	ms.each {
-		sh "cd ${it} && make bdd_tests"
-		//runTest(test:'bdd_tests')
+		sh "cd ${it} && make tests"
+		//runTest(test:'bdd_tests', True)
 	}
 }
