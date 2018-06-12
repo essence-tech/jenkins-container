f990ea8a8134328e4c59698b3d7545c12748937f
michel laporte <michel.laporte@L452MALT.local>
2018-06-12 11:55:00 +0100

    added push image

--- vars/starkillerDeploy.groovy
+++ vars/starkillerDeploy.groovy
@@ -5,10 +5,27 @@ def deployService(service, target = '') {
   }
 }
 
+def activate_service_account_gc(){
+   		sh 'gcloud auth activate-service-account --key-file=${JENKINS_HOME}${SECRETS_DIR}${SERVICE_ACCOUNT_FILE}'
+		}
+
 def buildImage(imageName, dockerfile) {
     def testImage = docker.build("${imageName}", "-f ${params.microServiceOption}/${dockerfile} .")
+}
 
-    // testImage.inside {
-    //     sh 'make test'
-    // }
+def buildImageAndTest(imageName, dockerfile) {
+    def testImage = docker.build("${imageName}", "-f ${params.microServiceOption}/${dockerfile} .")
+
+    testImage.inside {
+        sh 'make test'
+    }
+}
+
+def pushImageGcr(imageName) {
+    activate_service_account_gc()
+		sh '''
+		docker tag ${imageName} eu.gcr.io/${PROJECTNAME}/${params.microServiceOption}:${env.BUILD_NUMBER}
+		gcloud docker -- push eu.gcr.io/${PROJECTNAME}/${params.microServiceOption}:${env.BUILD_NUMBER}
+		'''
+
 }
