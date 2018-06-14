a4e4f17a14c58f8d1cbef188641d85b006558a17
michel laporte <michel.laporte@L452MALT.local>
2018-06-14 11:44:31 +0100

    -

--- vars/starkillerBuildImage.groovy
+++ vars/starkillerBuildImage.groovy
@@ -2,5 +2,5 @@ def buildImage(Closure body, Map config) {
 	body()
 	docker.image("${config.image}").inside("${DOCKER_SOCK_VOLUME} ${JENKINS_HOME}${SECRETS_DIR} -u root") {
 		sh 'docker ps'
-
+  }
 }
