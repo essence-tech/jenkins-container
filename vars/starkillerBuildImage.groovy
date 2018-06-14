2c4330a0e76563e6fd8f4fc564cd72f5c0bda14a
michel laporte <michel.laporte@L452MALT.local>
2018-06-14 11:51:49 +0100

    added docker

--- vars/starkillerBuildImage.groovy
+++ vars/starkillerBuildImage.groovy
@@ -1,6 +1,6 @@
-def buildImage(Closure body, image) {
+def buildImage(Closure body) {
 	body()
-	docker.image("${image}").inside("${DOCKER_SOCK_VOLUME} ${JENKINS_HOME}${SECRETS_DIR} -u root") {
+	docker.image('docker').inside("${DOCKER_SOCK_VOLUME} ${JENKINS_HOME}${SECRETS_DIR} -u root") {
 		sh 'docker ps'
   }
 }
