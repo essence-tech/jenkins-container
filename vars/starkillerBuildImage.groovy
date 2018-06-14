81c3511692eaef76d2f3b0b04adaf0724c7d9eb6
michel laporte <michel.laporte@L452MALT.local>
2018-06-14 15:05:36 +0100

    Added new docker build stage

--- vars/starkillerBuildImage.groovy
+++ vars/starkillerBuildImage.groovy
@@ -1,9 +1,19 @@
-def buildImage(body) {
+def buildServiceImage(body) {
 		def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 		docker.image("${config.image}").inside("${DOCKER_SOCK_VOLUME} -v ${JENKINS_HOME}${SECRETS_DIR}:${JENKINS_HOME}${SECRETS_DIR} -u root") {
-			sh 'docker ps'
+			starkillerGoogleOptions.activateServiceAccountGc()
+			buildImage("${config.registryRegion}/${PROJECTNAME}/${params.microServiceOption}:${env.BUILD_NUMBER}","${config.dockerfile}")
+      pushImageGcr("${params.microServiceOption}-${env.BUILD_NUMBER}")
   	}
+}
+
+def buildImage(imageName, dockerfile) {
+    def testImage = docker.build("${imageName}", "-f ${params.microServiceOption}/${dockerfile} .")
+}
+
+def pushImageGcr(imageName) {
+    sh "gcloud docker -- push eu.gcr.io/${env.PROJECTNAME}/${params.microServiceOption}:${env.BUILD_NUMBER}"
 }
--- vars/starkillerDeploy.groovy
+++ vars/starkillerDeploy.groovy
@@ -5,18 +5,10 @@ def deployService(service, target = '') {
   }
 }
 
-def buildImage(imageName, dockerfile) {
-    def testImage = docker.build("${imageName}", "-f ${params.microServiceOption}/${dockerfile} .")
-}
-
 def buildImageAndTest(imageName, dockerfile) {
     def testImage = docker.build("${imageName}", "-f ${params.microServiceOption}/${dockerfile} .")
 
     testImage.inside {
         sh 'make test'
     }
-}
-
-def pushImageGcr(imageName) {
-    sh "gcloud docker -- push eu.gcr.io/${env.PROJECTNAME}/${params.microServiceOption}:${env.BUILD_NUMBER}"
 }
