8fef41f6170c99385ffa73a06e17ffcc2a144488
michel laporte <michel.laporte@L452MALT.local>
2018-06-14 11:56:32 +0100

    double quotes

--- vars/starkillerBuildImage.groovy
+++ vars/starkillerBuildImage.groovy
@@ -1,6 +1,6 @@
 def buildImage(Closure body) {
 	body()
-	docker.image('eu.gcr.io/starkiller-184118/build-containers/build-image-alpine').inside("${DOCKER_SOCK_VOLUME} ${JENKINS_HOME}${SECRETS_DIR} -u root") {
+	docker.image("docker").inside("${DOCKER_SOCK_VOLUME} ${JENKINS_HOME}${SECRETS_DIR} -u root") {
 		sh 'docker ps'
   }
 }
