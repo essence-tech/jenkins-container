b064ef66f27f236e9ad493eb748ff19deaad6f39
michel laporte <michel.laporte@L452MALT.local>
2018-06-14 12:09:45 +0100

    config

--- vars/starkillerBuildImage.groovy
+++ vars/starkillerBuildImage.groovy
@@ -1,6 +1,9 @@
 def buildImage(Map config,Closure body) {
-	body()
-	docker.image("${config.image}").inside("${DOCKER_SOCK_VOLUME} ${JENKINS_HOME}${SECRETS_DIR} -u root") {
-		sh 'docker ps'
-  }
+	def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+		docker.image("${config.image}").inside("${DOCKER_SOCK_VOLUME} ${JENKINS_HOME}${SECRETS_DIR} -u root") {
+			sh 'docker ps'
+  	}
 }
